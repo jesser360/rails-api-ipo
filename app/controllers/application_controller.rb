2164837649dbb4932c908d333eac651e4f073ff6
jesse <jesser360@gmail.com>
2018-09-23 23:07:43 -0700

    commented out authernticate_request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,16 @@ class ApplicationController < ActionController::API
    # Add a before_action to authenticate all requests.
    # Move this to subclassed controllers if you only
    # want to authenticate certain methods.
-   
+
    # THIS IS WHERE WE AUTH REQUEST WITH JWT
-   # before_action :authenticate_request
+   before_action :authenticate_request
     attr_reader :current_user
 
     private
 
     def authenticate_request
-      @current_user = AuthorizeApiRequest.call(request.headers).result
-      render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+      # @current_user = AuthorizeApiRequest.call(request.headers).result
+      # render json: { error: 'Not Authorized' }, status: 401 unless @current_user
     end
 
 
