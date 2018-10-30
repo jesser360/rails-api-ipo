09f95e5a634c43d3c763cb63149cf533ba46a27b
jesse <jesser360@gmail.com>
2018-10-29 20:25:03 -0700

    image column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_28_202820) do
+ActiveRecord::Schema.define(version: 2018_10_30_032438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_10_28_202820) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "image"
     t.index ["user_id"], name: "index_artists_on_user_id"
   end
 
