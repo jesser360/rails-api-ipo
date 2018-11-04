7edce3a10caebdb1ba38bc02b85bfcc7a9e4d3c9
jesse <jesser360@gmail.com>
2018-11-04 15:04:10 -0800

    syntax

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::UsersController < ApplicationController
 
   def findUser
     @email = params[:email]
-    if User.where(email: @email.exists?
+    if User.where(email: @email).exists?
       render json: @user
     else
       render :nothing => true, :status => 204
